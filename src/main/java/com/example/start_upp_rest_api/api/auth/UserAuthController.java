03dd96f41540263ed3275651f832861091cd012b
AaronDeluna <aaron_deluna@mail.ru>
2024-10-11 19:46:14 +0300

    Перемещение и удаление контроллеров:
    - Перемещены контроллеры в пакет :
      -  из  в
      -  из  в
      -  в  с переименованием в
    
    - Удален .
    
    Помимо этих изменений, были модифицированы следующие файлы, но не добавлены в индекс:
    - , ,
    -

--- src/main/java/com/example/start_upp_rest_api/api/auth/UserAuthController.java
+++ src/main/java/com/example/start_upp_rest_api/api/auth/UserAuthController.java
@@ -1,8 +1,10 @@
-package com.example.start_upp_rest_api.controller;
+package com.example.start_upp_rest_api.controller.auth;
 
 import com.example.start_upp_rest_api.exception.UserNotFoundException;
+import com.example.start_upp_rest_api.exception.UserRegistrationException;
 import com.example.start_upp_rest_api.models.User;
 import com.example.start_upp_rest_api.service.UserAuthorizationService;
+import com.example.start_upp_rest_api.service.UserRegistrationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +12,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/authorization")
+@RequestMapping("/api/auth/")
 @AllArgsConstructor
-public class UserAuthorizationController {
+public class UserAuthController {
 
+    private final UserRegistrationService userRegistrationService;
     private final UserAuthorizationService userAuthorizationService;
 
-    @GetMapping
+    @PostMapping("register")
+    public ResponseEntity<String> registrationUser(@RequestBody User user) {
+        try {
+            userRegistrationService.registration(user);
+            return new ResponseEntity<>("Пользователь успешно зарегестрирован", HttpStatus.CREATED);
+        } catch (UserRegistrationException e) {
+            return new ResponseEntity<>("Ошибка: " + e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("login")
     public ResponseEntity<String> authorization(@RequestBody User user) {
         try {
             userAuthorizationService.authorization(user);
